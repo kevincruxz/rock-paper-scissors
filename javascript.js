de60de253de1c3c7ef0ce9e6f254d02b65960faf
Kevin Cruz <kevin_cruzz@outlook.es>
2022-02-28 18:28:48 -0600

    Added first version of html and modified js

--- javascript.js
+++ javascript.js
@@ -51,32 +51,5 @@ function game() {
     let computerWinsCount = 0;
     let playerSelection, computerSelection;
 
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Choose your weapon (rock, paper, scissors)");
-        playerSelection.toLowerCase();
-        computerSelection = computerPlay();
-        roundWinner = playRound(playerSelection, computerSelection);
-
-        if (roundWinner === 0) {
-            console.log("You Win! " + playerSelection + " beats " + computerSelection);
-            playerWinsCount++;
-        } else if (roundWinner === 1) {
-            console.log("Tie!");
-            i--;
-        } else if (roundWinner === 2) {
-            console.log("You Lose! " + computerSelection + " beats " + playerSelection);
-            computerWinsCount++;
-        } else {
-            console.log("Incorrect weapon.")
-            i--;
-        }
-
-        if (playerWinsCount === 3) {
-            console.log("You have Winned the match! :)")
-            break;
-        } else if (computerWinsCount === 3) {
-            console.log("You have lost the match! :(")
-            break;
-        }
-    }
+    
 }
